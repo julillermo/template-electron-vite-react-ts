54bdf87d8143339b542583543606ba362d3bf37b
TulioG_Projects <julius.guillermo.work@gmail.com>
2024-06-30 00:20:43 +0800

    match KuwenTho-Classic improvements

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Even if the port gets changed,
+    // it would seem that electron-vite is always looking port 5173
+    port: 5173,
+  }
 })
